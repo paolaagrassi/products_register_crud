6e4edf47cd8932b4b1570657e51203b9e7b16fe4
Paola <paola.grassi2014@gmail.com>
2022-10-18 18:16:31 -0300

    add packages

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,12 +1,19 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:products_register_crud/models/product_model.dart';
 
 class HomeViewController {
   late final Dio dio;
 
+  HomeViewController() {
+    final httpLogger = PrettyDioLogger();
+    dio = Dio()..interceptors.add(httpLogger);
+  }
+
   Future<List<ProductModel>?> getAllProducts() async {
     try {
       final response = await dio.get('http://192.168.1.7:8080/api/products');
+
       final rawList = response.data as List;
 
       return rawList
