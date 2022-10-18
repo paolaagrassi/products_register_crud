e1bac9e4cc34c66374c303521a464dd5bcab0672
Paola <paola.grassi2014@gmail.com>
2022-10-18 10:07:04 -0300

    create  product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+class ProductModel {
+  String id;
+  String name;
+  double price;
+  String description;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+  });
+
+  ProductModel copyWith({
+    String? id,
+    String? name,
+    double? price,
+    String? description,
+  }) {
+    return ProductModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      price: price ?? this.price,
+      description: description ?? this.description,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'price': price,
+      'description': description,
+    };
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      price: map['price'] as double,
+      description: map['description'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductModel.fromJson(String source) =>
+      ProductModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'ProductModel(id: $id, name: $name, price: $price, description: $description)';
+  }
+
+  @override
+  bool operator ==(covariant ProductModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.price == price &&
+        other.description == description;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ price.hashCode ^ description.hashCode;
+  }
+}
